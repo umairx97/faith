fa9f57e9b2bd741dc6a7138ec03725a8113500bc
Tech Fiyom Group <tech@uneleap.com>
2018-11-27 12:07:23 -0800

    update

--- android/app/src/main/java/com/faithmeetslove/MainApplication.java
+++ android/app/src/main/java/com/faithmeetslove/MainApplication.java
@@ -3,6 +3,7 @@ package com.faithmeetslove;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNI18nPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
             new LinearGradientPackage()
