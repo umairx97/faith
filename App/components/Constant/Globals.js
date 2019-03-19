3108340717cba546567e570389af4616fe552045
Manoj Sethi <manojsethi@live.in>
2019-03-19 09:47:36 +0530

    M. Sethi => Part 10

---
+++ App/components/Constant/Globals.js
@@ -0,0 +1,200 @@
+module.exports = {
+    geoLocationAPIKey: 'AIzaSyCE01q_TJTpclMAuj91Ozp-VC8M_RyrQvI',
+    STORE_KEY: 'a56z0fzrNpl^2',
+    BASE_URL: 'http://someurl.com',
+    mapStyle: [
+      {
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#242f3e"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#746855"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#242f3e"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#d59563"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#d59563"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#263c3f"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#6b9a76"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#38414e"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#212a37"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9ca5b3"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#746855"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#1f2835"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#f3d19c"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#2f3948"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.station",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#d59563"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#17263c"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#515c6d"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#17263c"
+          }
+        ]
+      }
+    ],
+    COLOR: {
+      ORANGE: '#C50',
+      DARKBLUE: '#0F3274',
+      LIGHTBLUE: '#6EA8DA',
+      DARKGRAY: '#999',
+    },
+    
+      theme:{
+      title: "#FFF",
+      more: "#446984",
+      center: "#7B8F99",
+      fullscreen: "transparent",
+      volume: "#A5957B",
+      scrubberThumb: "#234458",
+      scrubberBar: "#DBD5C7",
+      seconds: "#DBD5C7",
+      duration: "#DBD5C7",
+      progress: "#446984",
+      loading: "#DBD5C7"
+    },
+    theme1:{
+      title: "#FFF",
+      more: "#446984",
+      center: "#7B8F99",
+      fullscreen: "#446984",
+      volume: "#A5957B",
+      scrubberThumb: "#234458",
+      scrubberBar: "#DBD5C7",
+      seconds: "#DBD5C7",
+      duration: "#DBD5C7",
+      progress: "#446984",
+      loading: "#DBD5C7"
+    },
+    
+  };
